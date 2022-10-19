724afa85d023fec2cf44a5756a8e368c7be7ecea
전지은 <jiieun@hanyang.ac.kr>
2022-10-19 18:56:16 +0900

    로그인 xml 디자인 파일

---
+++ app/src/main/java/com/example/drizzle_prj/SearchListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drizzle_prj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_list)
+    }
+}
---
+++ app/src/main/java/com/example/drizzle_prj/StoreListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drizzle_prj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_list)
+    }
+}
