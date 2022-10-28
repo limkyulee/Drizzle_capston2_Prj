5b0932b2435978f2f8297d73d531175f65b7ca24
JjjjjjAaaaaaHhhhhh <020209jah@naver.com>
2022-10-28 11:37:38 +0900

    로그인 xml 디자인 파일

--- app/src/main/java/com/example/drizzle_prj/LOGIN0/App.kt
+++ app/src/main/java/com/example/drizzle_prj/LOGIN0/App.kt
@@ -3,10 +3,11 @@ package com.example.drizzle_prj.LOGIN0
 import android.app.Application
 
 class App : Application() {
+    /*
     override fun OnCreate() {
         super.OnCreate()
         instance = this
-    }
+    }*/
 
     companion object {
         lateinit var instance : App
