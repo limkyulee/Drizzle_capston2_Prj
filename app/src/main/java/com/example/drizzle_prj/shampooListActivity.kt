d883be11d89d9c90504443926cee4d2bb5c1f227
전지은 <jiieun@hanyang.ac.kr>
2022-11-04 19:17:38 +0900

    xml 코드

---
+++ app/src/main/java/com/example/drizzle_prj/shampooListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drizzle_prj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class shampooListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shampoo_list)
+    }
+}
