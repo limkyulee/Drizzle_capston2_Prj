37305f9368dc54ac19faab9e6e1bff981c19cf96
전지은 <jiieun@hanyang.ac.kr>
2022-11-05 16:04:57 +0900

    main 아이콘 리스트 페이지

---
+++ app/src/main/java/com/example/drizzle_prj/NoteListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drizzle_prj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+    }
+}
